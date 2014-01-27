28cc0cee3b015a66bfa97bdfce1c65f2e12f9cbd
Kent William Innholt <kinnholt@fanhattan.com>
2014-01-26 18:16:25 -0800

    Added simple backend account creation

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,20 @@ class AccountsController < ApplicationController
   end
 
   def create
+    # Get the credit card details submitted by the form
+    token = params[:stripe_token]
+
+    # Create a Customer
+    customer = Stripe::Customer.create(
+      card: token,
+      plan: "monthly",
+      email: params[:email],
+      metadata: {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+
     render text: 'creating!'
   end
 end
