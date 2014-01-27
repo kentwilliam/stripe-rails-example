76ce961592003a9dfec7fe03e7670645444235a6
Kent William Innholt <kinnholt@fanhattan.com>
2014-01-26 16:19:25 -0800

    Initialized basic Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StripeRailsExample::Application.config.secret_key_base = '4c94ae3f9062d1a8c69158e74bf5113ab63912c231945d1b9c6e393b923338130e8e61b8ad7a8c33c67a5f03650cc4840b8e4d4e61b363f926bd52dc0c407281'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StripeRailsExample::Application.config.session_store :cookie_store, key: '_stripe-rails-example_session'
